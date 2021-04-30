e6395da2fb2bd56dbc55acb38b34c0d2da18b448
azuck <azuck@10.0.0.87>
2021-04-29 22:17:58 -0400

    Created a class for a the stadium to incorporate major elements. Didn't do much else with it.

---
+++ CS-173_FinalProject/src/cs/pkg173_finalproject/Stadium.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs.pkg173_finalproject;
+
+/**
+ *
+ * @author azuck
+ */
+public class Stadium {
+    private int numSeats;
+    private char section;
+    private String seatCategory;
+}
