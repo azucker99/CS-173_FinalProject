5f3e1f18f998491c62ae6c4617f738280688c263
azuck <azuck@10.0.0.87>
2021-05-07 12:30:26 -0400

    Added more to the stadium class and the main class.

--- CS-173_FinalProject/src/cs/pkg173_finalproject/Main.java
+++ CS-173_FinalProject/src/cs/pkg173_finalproject/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package cs.pkg173_finalproject;
+import java.util.Scanner;
 
 /**
  *
@@ -17,6 +18,30 @@ public class Main {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        makeNewStadium
+        
+        
+        
+        
+        
     }
     
+  /* public static double getPrice(String game, String seatCategory, char section,  
+    */
+    public static Stadium makeNewStadium() {
+        Stadium stadium = new Stadium();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose a game:");
+        // add do loop
+        String game = scanner.nextLine();
+        System.out.println("Please choose a seat category:");
+        String seatCategory = scanner.nextLine();
+        System.out.println("Please choose a seat section:");
+        String section = scanner.next();
+        System.out.println("Please select how many tickets you would like:");
+        int seats = scanner.nextInt(); 
+        stadium.getSection().add(section);
+        return stadium;
+        }
 }
--- CS-173_FinalProject/src/cs/pkg173_finalproject/Stadium.java
+++ CS-173_FinalProject/src/cs/pkg173_finalproject/Stadium.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package cs.pkg173_finalproject;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,6 +12,87 @@ package cs.pkg173_finalproject;
  */
 public class Stadium {
     private int numSeats;
-    private char section;
+    private ArrayList<String> section;
     private String seatCategory;
+    private ArrayList<Integer> seatAvailability;
+    private ArrayList<String> schedule;
+    
+public Stadium () {
+    this.numSeats = 32;
+    this.schedule = new ArrayList<String>();
+    this.section = new ArrayList<String>();
+    
+}
+
+    /**
+     * @return the numSeats
+     */
+    public int getNumSeats() {
+        return numSeats;
+    }
+
+    /**
+     * @param numSeats the numSeats to set
+     */
+    public void setNumSeats(int numSeats) {
+        this.numSeats = numSeats;
+    }
+
+    /**
+     * @return the section
+     */
+    public ArrayList<String> getSection() {
+        return section;
+    }
+
+    /**
+     * @param section the section to set
+     */
+    public void setSection(ArrayList<String> section) {
+        this.section = section;
+    }
+
+    /**
+     * @return the seatCategory
+     */
+    public String getSeatCategory() {
+        return seatCategory;
+    }
+
+    /**
+     * @param seatCategory the seatCategory to set
+     */
+    public void setSeatCategory(String seatCategory) {
+        this.seatCategory = seatCategory;
+    }
+
+    /**
+     * @return the seatAvailability
+     */
+    public ArrayList<Integer> getSeatAvailability() {
+        return seatAvailability;
+    }
+
+    /**
+     * @param seatAvailability the seatAvailability to set
+     */
+    public void setSeatAvailability(ArrayList<Integer> seatAvailability) {
+        this.seatAvailability = seatAvailability;
+    }
+
+    /**
+     * @return the Schedule
+     */
+    public ArrayList<String> getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * @param Schedule the Schedule to set
+     */
+    public void setSchedule(ArrayList<String> Schedule) {
+        this.schedule = Schedule;
+    }
+
+    
 }
